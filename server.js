22227912c9b40a1a362804c6cdf25c9f307b101b
xandjiji <xandjiji@gmail.com>
2020-08-24 13:24:28 -0300

    using template engine

--- server.js
+++ server.js
@@ -1,7 +1,15 @@
 const express = require('express');
 const app = express();
 
+const fs = require('fs').promises;
 const { appendComment } = require('./crud');
+
+const useTemplateEngine = false;
+
+
+if(useTemplateEngine) {
+    app.set('view engine', 'ejs');
+}
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
@@ -11,8 +19,16 @@ app.listen(3000, () => {
     console.log('---> http://localhost:3000/');
 });
 
-app.get('/', (req, res) => {
-    res.sendFile(`${__dirname}/index.html`);
+app.get('/', async (req, res) => {
+
+    if(useTemplateEngine) {
+        let data = await fs.readFile(`comments.json`, 'utf-8');
+        data = JSON.parse(data);
+        res.render('index.ejs', { comments: data });
+    } else {
+        res.sendFile(`${__dirname}/index.html`);
+    }
+
 });
 
 app.post('/addComment', async (req, res) => {
